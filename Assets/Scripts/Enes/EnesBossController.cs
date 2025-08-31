266795bdf49e16069d6cf4f3a86f244255cba1b7
PainStar69 <uchihahapain@gmail.com>
2025-08-31 20:33:01 +0300

    Added Skill Enes

--- Assets/Scripts/Enes/EnesBossController.cs
+++ Assets/Scripts/Enes/EnesBossController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class EnesBossController : MonoBehaviour
 {
+    [Header("Prefab's")]
+    [SerializeField] private GameObject _magicCircle;
 
-    void Start()
+    [Header("Int's")]
+    [SerializeField] private int _attackCount;
+
+    [Header("GameObject's")]
+    private GameObject _character;
+
+    void Awake()
     {
-        
+        _character = GameObject.Find("Player");
     }
 
 
     void Update()
     {
-        
+        if(_attackCount == 0)
+        {
+            GameObject _magic =Instantiate(_magicCircle, _character.transform.position, Quaternion.identity);
+            _attackCount = 1;
+        }
     }
 }
