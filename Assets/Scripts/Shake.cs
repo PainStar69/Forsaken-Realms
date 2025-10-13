9ada511432b2f5e20b2e0409bf5f0480d378a93e
PainStar69 <uchihahapain@gmail.com>
2025-10-14 00:21:04 +0300

    Fix

--- Assets/Scripts/Shake.cs
+++ Assets/Scripts/Shake.cs
@@ -3,47 +3,46 @@ using UnityEngine;
 
 public class Shake : MonoBehaviour
 {
-    public float intensity = 0.1f;   // Titreme büyüklüðü
-    public float duration = 0.5f;    // Titreme süresi
+    [Header("Float's")]
+    public float _intensity = 0.1f;
+    public float _duration = 0.5f;
+    private float _currentShakeTime = 0f;
 
-    private Vector3 originalPos;
-    private float currentShakeTime = 0f;
-    private bool isShaking = false;
+    [Header("Vector's")]
+    private Vector3 _originalPos;
+
+    [Header("Bool's")]
+    private bool _isShaking = false;
 
     void Start()
     {
-        originalPos = transform.localPosition;
+        _originalPos = transform.localPosition;
     }
 
     public void ShakeStart()
     {
-        // Titreme süresini resetle
-        currentShakeTime = duration;
+        _currentShakeTime = _duration;
 
-        // Eðer zaten titremiyorsa, baþlat
-        if (!isShaking)
+        if (!_isShaking)
             StartCoroutine(ShakeCoroutine());
     }
 
     private IEnumerator ShakeCoroutine()
     {
-        isShaking = true;
+        _isShaking = true;
 
-        while (currentShakeTime > 0f)
+        while (_currentShakeTime > 0f)
         {
-            // Her frame yeni pozisyon üret
-            float offsetX = Random.Range(-intensity, intensity);
-            float offsetY = Random.Range(-intensity, intensity);
+            float _offsetX = Random.Range(-_intensity, _intensity);
+            float _offsetY = Random.Range(-_intensity, _intensity);
 
-            transform.localPosition = originalPos + new Vector3(offsetX, offsetY, 0f);
+            transform.localPosition = _originalPos + new Vector3(_offsetX, _offsetY, 0f);
 
-            // Süreyi azalt
-            currentShakeTime -= Time.deltaTime;
+            _currentShakeTime -= Time.deltaTime;
             yield return null;
         }
 
-        // Bitince pozisyonu sýfýrla
-        transform.localPosition = originalPos;
-        isShaking = false;
+        transform.localPosition = _originalPos;
+        _isShaking = false;
     }
 }
